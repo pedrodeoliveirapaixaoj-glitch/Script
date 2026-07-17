d717403684a3dcbeb909bb4cf52d25325889e3d0
pedrodeoliveirapaixaoj-glitch <pedrodeoliveirapaixaoj@gmail.com>
2026-07-17 15:05:23 -0300

    Create Homem-aranha.lua

---
+++ Homem-aranha.lua
@@ -0,0 +1,18 @@
+ReplicatedStorage
+├── Modules
+│   ├── Superman.lua
+│   ├── SpiderMan.lua
+│   └── Config.lua
+├── RemoteEvents
+│   ├── SelectHero
+│   └── UsePower
+
+StarterGui
+└── HeroGUI
+
+StarterPlayer
+└── StarterPlayerScripts
+    └── HeroClient.lua
+
+ServerScriptService
+└── HeroServer.lua
