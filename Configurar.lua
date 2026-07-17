2e62f74c5e3e7876ca36124e181eb3ecaff22a5d
pedrodeoliveirapaixaoj-glitch <pedrodeoliveirapaixaoj@gmail.com>
2026-07-17 15:06:57 -0300

    Create Configurar.lua

---
+++ Configurar.lua
@@ -0,0 +1,15 @@
+local Config = {}
+
+Config.Superman = {
+    WalkSpeed = 40,
+    JumpPower = 100,
+    MaxHealth = 300
+}
+
+Config.SpiderMan = {
+    WalkSpeed = 28,
+    JumpPower = 70,
+    MaxHealth = 200
+}
+
+return Config
